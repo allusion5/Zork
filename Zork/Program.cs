c87127fee4dcebc81eb57b30d00116d26bf1ef85
HawkManHawkPlan <60548332+HawkManHawkPlan@users.noreply.github.com>
2022-09-12 14:53:03 -0400

    Zork 2.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,13 +4,18 @@ namespace Zork
 {
 	class Program
 	{
+		private static string CurrentRoom
+		{
+			get { return _rooms[_currentRow, _currentColumn]; }
+		}
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to Zork!");
 			bool isRunning = true;
 			while (isRunning)
 			{
-				Console.WriteLine($"{_rooms[_currentRoom]}\n> ");
+				Console.WriteLine(CurrentRoom);
+				Console.Write("> ");
 				string inputString = Console.ReadLine().Trim();
 				Commands command = ToCommand(inputString);
 				string outputString;
@@ -37,7 +42,7 @@ namespace Zork
 						}
 						break;
 					default:
-						outputString = $"Unrecognized command: {command}";
+						outputString = $"Unrecognized command: {inputString.ToUpper()}";
 						break;
 				}
 				Console.WriteLine(outputString);
@@ -55,26 +60,37 @@ namespace Zork
 			}
 		}
 
-		private static bool Move(Commands command)
+		private static bool Move(Commands direction)
 		{
-			bool didMove=false;
-			switch (command)
+			bool didMove = false;
+			switch (direction)
 			{
-				case Commands.NORTH:
-				case Commands.SOUTH:
-					break;
-				case Commands.EAST when _currentRoom < _rooms.Length - 1:
-					_currentRoom++;
+				case Commands.NORTH when _currentRow < _rooms.GetLength(0) - 1:
+					_currentRow++;
 					didMove = true;
 					break;
-				case Commands.WEST when _currentRoom > 0:
-					_currentRoom--;
+				case Commands.SOUTH when _currentRow > 0:
+					_currentRow--;
+					didMove = true;
+					break;
+				case Commands.EAST when _currentColumn < _rooms.GetLength(1) - 1:
+					_currentColumn++;
+					didMove = true;
+					break;
+				case Commands.WEST when _currentColumn > 0:
+					_currentColumn--;
 					didMove = true;
 					break;
 			}
 			return didMove;
 		}
-		private static readonly string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-		private static int _currentRoom = 1;
+		private static readonly string[,] _rooms =
+		{
+			{"Rocky Trail", "South of House", "Canyon View" },
+			{"Forest", "West of House", "Behind House"},
+			{"Dense Woods", "North of House","Clearing" }
+		};
+		private static int _currentRow = 1;
+		private static int _currentColumn = 1;
 	}
 }
